d797011f496226ba8694e9bf6370e5c0a97bfb4d
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2024-04-12 00:09:18 +0100

    feat: demonstrating options API syntax

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <template>
+  <div class="app">
+    <p>{{ name }} - {{ age }}</p>
+    <button @click="changeName('Chikamso')">Change Name</button>
+    <button @click="changeAge(32)">Change Age</button>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-
 export default defineComponent({
   name: 'App',
-  components: {
-    
+  components: {},
+  data() {
+    return {
+      name: 'Link',
+      age: 25 as string | number
+    }
+  }, 
+  methods: {
+    changeName(name: string) {
+      this.name = name
+      return name
+    },
+    changeAge(age: string | number) {
+      this.age = age
+      return age
+    }
   }
 });
 </script>
